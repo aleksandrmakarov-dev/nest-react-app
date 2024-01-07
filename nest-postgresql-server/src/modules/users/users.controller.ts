429e64159d1bff81509f047519380f78deea76ef
Alex <ac5295@student.jamk.fi>
2024-01-07 13:01:57 +0200

    add and remove favorite artiles

--- nest-postgresql-server/src/modules/users/users.controller.ts
+++ nest-postgresql-server/src/modules/users/users.controller.ts
@@ -1,7 +1,25 @@
-import { Controller, Get } from "@nestjs/common";
+import {
+  Body,
+  ConflictException,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { UserResponseDto } from "./dto/user-response.dto";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { MakeFavoriteDto } from "./dto/make-favorite.dto";
+import { User } from "../auth/decorators/user.decorator";
+import { JwtPayloadDto } from "../auth/dto/jwt-payload.dto";
+import { Role } from "@prisma/client";
+import { Authorize } from "../auth/decorators/authorize.decorator";
+import { GenericResponseDto } from "src/common/dto/response.dto";
 
 @ApiTags("users")
 @Controller("users")
@@ -16,5 +34,69 @@ export class UsersController {
     return {
       items: users,
     };
+  }
+
+  @Post(":id/favorite")
+  @ApiBearerAuth()
+  @Authorize(Role.EDITOR, Role.ADMIN)
+  async makeFavorite(
+    @User() user: JwtPayloadDto,
+    @Param("id") id: string,
+    @Body() dto: MakeFavoriteDto,
+  ) {
+    if (id !== user.id && user.role !== Role.ADMIN) {
+      throw new UnauthorizedException(
+        "Only ADMIN user can create resource and assign it to another user",
+      );
+    }
+
+    const foundUser = await this.usersService.findById(id);
+
+    if (!foundUser) {
+      throw new NotFoundException(
+        "User that you want assign to resource not found",
+      );
+    }
+
+    const hasFavorite = await this.usersService.hasFavorite(id, dto.articleId);
+
+    if (hasFavorite) {
+      throw new ConflictException("Article is already in favorites");
+    }
+
+    await this.usersService.makeFavorite(id, dto.articleId);
+
+    const response: GenericResponseDto = {
+      message: "Made favorite",
+    };
+
+    return response;
+  }
+
+  @Get(":id/favorite")
+  async favorites(@Param("id") id: string) {
+    return await this.usersService.favorites(id);
+  }
+
+  @Delete(":id/favorite/:articleId")
+  @ApiBearerAuth()
+  @Authorize(Role.EDITOR, Role.ADMIN)
+  async deleteFavorite(
+    @User() user: JwtPayloadDto,
+    @Param("id") id: string,
+    @Param("articleId") articleId: string,
+  ) {
+    if (id !== user.id && user.role !== Role.ADMIN) {
+      throw new UnauthorizedException(
+        "Only user who make resource favorite or ADMIN user can delete it",
+      );
+    }
+
+    const hasFavorite = await this.usersService.hasFavorite(id, articleId);
+    if (!hasFavorite) {
+      throw new ConflictException("User does not have article in favorites");
+    }
+
+    await this.usersService.deleteFavorite(id, articleId);
   }
 }
--- nest-postgresql-server/src/modules/users/users.service.ts
+++ nest-postgresql-server/src/modules/users/users.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { DatabaseService } from "src/core/database/database.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { EmailVerificationTokenDto } from "./dto/email-verification-token.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { BcryptService } from "src/core/bcrypt/bcrypt.service";
+import { MakeFavoriteDto } from "./dto/make-favorite.dto";
+import { NotFoundError } from "rxjs";
 
 @Injectable()
 export class UsersService {
@@ -75,5 +77,68 @@ export class UsersService {
         id: id,
       },
     });
+  }
+
+  async makeFavorite(id: string, articleId: string) {
+    return await this.databaseService.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        favoriteArticles: {
+          connect: {
+            id: articleId,
+          },
+        },
+      },
+    });
+  }
+
+  async deleteFavorite(id: string, articleId: string) {
+    return await this.databaseService.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        favoriteArticles: {
+          disconnect: {
+            id: articleId,
+          },
+        },
+      },
+    });
+  }
+
+  async favorites(id: string) {
+    return await this.databaseService.article.findMany({
+      where: {
+        userFavorite: {
+          some: {
+            id: id,
+          },
+        },
+      },
+    });
+  }
+
+  async hasFavorite(id: string, articleId: string) {
+    const foundUser = await this.databaseService.user.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        favoriteArticles: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+
+    if (!foundUser) {
+      throw new NotFoundException("User not found");
+    }
+
+    return foundUser.favoriteArticles.map((v) => v.id).includes(articleId);
   }
 }
