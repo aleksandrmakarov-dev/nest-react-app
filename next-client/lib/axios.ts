c805591e5bf9b903ccdde46f26c9a75449894e4d
Alex <ac5295@student.jamk.fi>
2024-01-22 21:56:38 +0200

    vercel conf

--- next-client/lib/axios.ts
+++ next-client/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const config = {
-  baseURL: "http://localhost:3001/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
